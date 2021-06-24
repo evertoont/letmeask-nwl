b5a6ac091484f7995b742b11cfa7e2bed5a7376e
Everton Carmo <evertoontcarmo@gmail.com>
2021-06-24 18:45:31 -0300

    Changes route after room creation

--- src/pages/NewRoom/index.tsx
+++ src/pages/NewRoom/index.tsx
@@ -37,7 +37,7 @@ export function NewRoom() {
       authorId: user?.id,
     });
 
-    history.push(`/rooms/${firebaseRoom.key}`);
+    history.push(`/admin/rooms/${firebaseRoom.key}`);
   }
 
   return (
